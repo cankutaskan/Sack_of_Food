3905f117b248e47f58cb8a4d1aededa579059d09
Nealora <gokhan.nas@windowslive.com>
2016-11-29 17:55:25 +0300

    Added simple login and logout system.
    Added login, logout.
    Changed Users.Email's annotation from required to not required to
    prevent validation issue.

--- SOF301/SOF301/Controllers/AuthController.cs
+++ SOF301/SOF301/Controllers/AuthController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SOF301.Models;
+using System.Security.Claims;
 
 namespace SOF301.Controllers
 {
@@ -10,9 +12,56 @@ namespace SOF301.Controllers
     public class AuthController : Controller
     {
         // GET: Auth
-        public ActionResult Login()
+        [HttpGet]
+        public ActionResult Login(string url)
         {
+            
             return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(Users model)
+        {
+            if (!ModelState.IsValid) //Checks if input fields have the correct format
+            {
+
+                Response.Write("model not valid");
+                return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+            }
+
+            int userId = new SofModel().Users
+                .Where(u => u.UserName == model.UserName && u.Password == model.Password)
+                .Select(u => u.UserID).FirstOrDefault();
+            if (userId != 0)
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Sid, userId.ToString()),
+                    new Claim(ClaimTypes.Sid, new SofModel().Users.Where(u=>u.UserID == userId).Select(u=>u.RoleID).FirstOrDefault().ToString())
+                }, "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                authManager.SignIn(identity);
+                
+                
+
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password!");
+                return View(model);
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Login", "Auth");
         }
     }
 }
