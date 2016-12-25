8f77dba6e9e5b4c8fb78afc632ebe80201d7f97f
cankutaskan <cankutaskan@hotmail.com>
2016-12-25 15:33:43 +0300

    Customer dispose fix

--- SOF301/SOF301/Controllers/CustomerController.cs
+++ SOF301/SOF301/Controllers/CustomerController.cs
@@ -214,14 +214,7 @@ namespace SOF301.Controllers
             return View(list);
         }
         
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                SOFEntity.getDb().Dispose();
-            }
-            base.Dispose(disposing);
-        }
+ 
 
     }
 }
